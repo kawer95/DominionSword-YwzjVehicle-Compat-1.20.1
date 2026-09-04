d75b8497825c3959084ad517096434d4fae172f1
kawer95 <1034765569@qq.com>
2026-09-04 22:06:49 +0800

    fix: release YWZJ camera in command mode

---
+++ src/main/java/com/arxyt/dominionsword/ywzjvehiclecompat/mixin/VehicleMouseIsolationMixin.java
@@ -0,0 +1,18 @@
+package com.arxyt.dominionsword.ywzjvehiclecompat.mixin;
+
+import com.arxyt.dominionsword.client.ClientSpirit;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.ywzj.vehicle.vehicle.LocalVehiclePlayer;
+
+/** Lets Dominion's command-camera input run instead of YWZJ's mounted-view mouse lock. */
+@Mixin(value = LocalVehiclePlayer.class, remap = false)
+public abstract class VehicleMouseIsolationMixin {
+    @Inject(method = "handlePlayerTurn", at = @At("HEAD"), cancellable = true)
+    private void dominionsword$releaseCommandCameraMouse(double yaw, double pitch,
+                                                         CallbackInfoReturnable<Boolean> cir) {
+        if (ClientSpirit.active()) cir.setReturnValue(false);
+    }
+}
